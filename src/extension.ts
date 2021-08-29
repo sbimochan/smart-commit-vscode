35a9c0ab953d670404d8266249ea6666ed64b2c3
Bimochan Shrestha <bmochan@gmail.com>
2021-08-29 11:38:20 +0545

    Enable custom source control and show source control menu on load

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('smart-commit-vscode.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('smart-commit-vscode.smartCommit', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Smart Commit!');
+		const smartCommitSCM = vscode.scm.createSourceControl('smart-commit','smart commit');
+		vscode.commands.executeCommand('workbench.view.scm');
+		smartCommitSCM.acceptInputCommand;
+		// vscode.
+		// smartCommitSCM.acceptInputCommand?.command
+
 	});
 
 	context.subscriptions.push(disposable);
